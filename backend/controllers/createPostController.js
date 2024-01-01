4b3d57d103894e7a016decf26f3a790f41d31bc1
Abdulazeez Abdulhakeem <hakymazeez@gmail.com>
2024-01-01 23:53:15 +0100

    added dayjs for createdAt of post

--- backend/controllers/createPostController.js
+++ backend/controllers/createPostController.js
@@ -1,16 +1,21 @@
 import expressAsyncHandler from "express-async-handler";
 import User from "../models/User.js";
 import Post from "../models/Post.js";
+import dayjs from "dayjs";
 
 export const createPost = expressAsyncHandler(async (req, res) => {
-  const {description, imageUrl, userId } = req.body
-  
-if(!findUser) return res.status(404).json('user not found')
+  const { description, imageUrl, userId } = req.body;
 
-  
+  if (!findUser) return res.status(404).json("user not found");
+  const currentDate = dayjs();
+  const formattedDate = currentDate.format("YYYY-MM-DD HH:mm:ss");
+
   const newPost = await Post.create({
-    description, imageUrl, createdAt:Date.now(), postedBy:userId
-  })
+    description,
+    imageUrl,
+    createdAt: formattedDate,
+    postedBy: userId,
+  });
 
-  res.status(201).json({status: 'created', post: newPost})
-})
+  res.status(201).json({ status: "created", post: newPost });
+});
